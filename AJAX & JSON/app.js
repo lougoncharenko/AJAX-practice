96861355224605c4ccf74351ce2bd49d2b7e17ef
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-01-26 13:40:51 -0500

    Add files via upload
    this file shows how to work with local APIs in JavaScript using AJAX & JSON

---
+++ AJAX & JSON/app.js
@@ -0,0 +1,61 @@
+document.getElementById('button1').addEventListener('click',loadCustomer);
+
+function loadCustomer(e){
+    const xhr= new XMLHttpRequest();
+
+    xhr.open('GET', "customer.json", true);
+    
+
+    xhr.onload=function(){
+        if(this.status ===200){
+            // console.log(this.responseText)
+            const customer = JSON.parse(this.responseText);
+            
+            const output = `
+            <ul>
+              <li>ID: ${customer.id}</li>
+              <li>Name: ${customer.name}</li>
+              <li>Company: ${customer.company}</li>
+              <li>Phone: ${customer.phone}</li>
+            </ul>
+          `;
+        
+        document.getElementById('customer').innerHTML = output;
+    }
+}
+    xhr.send();
+}
+
+
+// load customers
+document.getElementById("button2").addEventListener('click', loadCustomers);
+
+function loadCustomers(e){
+const xhr=new XMLHttpRequest();
+
+xhr.open("GET", 'customers.json', true);
+
+xhr.onload=function(){
+    if(this.status ===200){
+        const customers=JSON.parse(this.responseText);
+         
+        let output= '';
+        
+         customers.forEach(function(customer){
+        //the += allows for us to append instead of override and assign
+            output += ` 
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+       });
+        
+        
+      document.getElementById('customers').innerHTML = output;
+    }
+}
+xhr.send();
+}
