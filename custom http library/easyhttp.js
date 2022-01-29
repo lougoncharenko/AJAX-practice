37c7e3812341195f0e49c68a5af91492762b54e4
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-01-29 17:38:34 -0500

    Add files via upload

--- custom http library/easyhttp.js
+++ custom http library/easyhttp.js
@@ -7,31 +7,35 @@ function easyHTTP(){
 }
 
 //make an http get request
+// easyHTTP.prototype.get=function(url, callback){
+// this.http.open('GET', url, true);
+// //use arrow function to let it work because this.http is not defined in this function scope but arrow functions use lexical scope
+// //A lexical scope in JavaScript means that a variable defined outside a function can be accessible inside another function defined after the variable declaration.
+// this.http.onload= () => {
+//     if(this.http.status===200){
+//         callback(null, this.http.responseText); 
+//     } else{
+//         callback('Error: ' + self.http.status);
+//     }
+// }
+// this.http.send();
+
+
 easyHTTP.prototype.get=function(url, callback){
-this.http.open('GET', url, true);
-//use arrow function to let it work because this.http is not defined in this function scope but arrow functions use lexical scope
-//A lexical scope in JavaScript means that a variable defined outside a function can be accessible inside another function defined after the variable declaration.
-this.http.onload= () => {
-    if(this.http.status===200){
-        callback(null, this.http.responseText); 
-    } else{
-        callback('Error: ' + self.http.status);
+    this.http.open('GET', url, true);
+
+    let self=this;
+    this.http.onload= () => {
+        if(self.http.status === 200){
+           callback(null, self.http.responseText);
+    }else{
+        callback('Error:' +self.http.status);
     }
 }
-this.http.send();
+    this.http.send();
+    
+
 }
-
-// easyHTTP.prototype.get=function(url){
-//     this.http.open('GET', url, true);
-//     let self=this;
-//     this.http.onload= () => {
-//         if(self.http.status === 200){
-//             return self.http.responseText;
-//         }
-//     }
-//     this.http.send();
-//     }
-
 //make an http post request
 //make an http put request
 //make an http delete request
