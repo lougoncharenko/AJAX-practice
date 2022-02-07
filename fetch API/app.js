d426125d0041485cdbf82b9b7ad2939998b8c6cb
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-07 12:27:39 -0500

    Add files via upload

--- fetch API/app.js
+++ fetch API/app.js
@@ -1,5 +1,7 @@
 //fetch api
 
+
+//using a get request with txt
 document.getElementById('button1').addEventListener('click', getText);
 
 function getText(){
@@ -10,5 +12,28 @@ function getText(){
     })
     .then(function(data){
         console.log(data);
-    });
+        document.getElementById('output').innerHTML=data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+
+//get local JSON data
+document.getElementById('button2').addEventListener('click', getJson);
+
+function getJson(){
+    fetch('post.json')
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 }
