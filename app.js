643436608a0ad442c06d6e68db0492ba49b7010b
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-07 11:32:51 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 //fetch api
 
+
+//using a get request with txt
 document.getElementById('button1').addEventListener('click', getText);
 
 function getText(){
-    fetch('test.text')
+    fetch('text.txt')
     .then(function(response){
         console.log(response)
-    });
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('output').innerHTML=data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 }
+
